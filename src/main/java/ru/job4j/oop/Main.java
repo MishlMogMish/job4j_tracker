199fdd6f562b25f2365aeb3629932020d9b85a28
Мик <mishlmish@inbox.ru>
2023-09-03 15:00:05 +0300

    6.0. Абстрактные классы [#504949 #442018]

---
+++ src/main/java/ru/job4j/oop/Main.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        truck.accelerate();
+        truck.steer();
+        truck.brake();
+        System.out.println();
+
+        Athlete athlete = new Athlete();
+        athlete.run();
+        athlete.sprint();
+        System.out.println();
+
+        FootballPlayer footballPlayer = new FootballPlayer();
+        footballPlayer.run();
+        footballPlayer.footKick();
+    }
+}
