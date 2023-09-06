06cb39ecd6d1b659f2270f33fcc1ef7d8a56bc25
Мик <mishlmish@inbox.ru>
2023-09-07 00:11:51 +0300

    разбор

--- src/main/java/ru/job4j/zeal/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/zeal/ru/job4j/tracker/Tracker.java
@@ -1,32 +1,27 @@
 package ru.job4j.zeal.ru.job4j.tracker;
 
+import ru.job4j.tracker.Item;
+
 import java.util.Arrays;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    private final ru.job4j.tracker.Item[] items = new ru.job4j.tracker.Item[100];
     private int ids = 1;
     private int size = 0;
 
-    public Item add(Item item) {
+    public ru.job4j.tracker.Item add(ru.job4j.tracker.Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+    public ru.job4j.tracker.Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
-    public Item[] findByName(String key) {
-        Item[] temp = new Item[size];
+    public ru.job4j.tracker.Item[] findByName(String key) {
+        ru.job4j.tracker.Item[] temp = new ru.job4j.tracker.Item[size];
         int nameIndex = 0;
         for (int i = 0; i < size; i++) {
             if (key.equals(items[i].getName())) {
@@ -36,32 +31,82 @@ public class Tracker {
         return Arrays.copyOf(temp, nameIndex);
     }
 
-    public  Item[] findAll() {
-        Item[] temp = new Item[size];
+    public ru.job4j.tracker.Item[] findAll() {
+        ru.job4j.tracker.Item[] temp = new ru.job4j.tracker.Item[size];
         for (int i = 0; i < size; i++) {
             temp[i] = items[i];
         }
         return temp;
     }
 
+    public boolean replace(int id, ru.job4j.tracker.Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = item;
+            items[index].setId(id);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - 1 - index);
+            items[size - 1] = null;
+            size--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
-        Item first = new Item("First");
-        Item second = new Item("Second");
-        Item third = new Item("Third");
-        tracker.add(first);
-        tracker.add(new Item("Second"));
-        tracker.add(second);
-        tracker.add(third);
-        tracker.add(new Item());
-        tracker.add(new Item(25, "Broker"));
+        ru.job4j.tracker.Item item1 = new ru.job4j.tracker.Item("a");
+        ru.job4j.tracker.Item item2 = new ru.job4j.tracker.Item("b");
+        ru.job4j.tracker.Item item3 = new ru.job4j.tracker.Item("c");
+        ru.job4j.tracker.Item item4 = new ru.job4j.tracker.Item("d");
+        ru.job4j.tracker.Item item5 = new ru.job4j.tracker.Item("e");
+        ru.job4j.tracker.Item item6 = new ru.job4j.tracker.Item("f");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        tracker.add(item5);
+        tracker.add(item6);
 
-        for (Item item : tracker.findAll()) {
-            System.out.println("id " + item.getId() + " name " + item.getName());
+        for (int i = 0; i < tracker.size; i++) {
+            System.out.println(tracker.items[i].getName() + "\t  " + tracker.items[i].getId());
         }
         System.out.println();
-        for (Item item : tracker.findByName("Second")) {
-            System.out.println("id " + item.getId() + " name " + item.getName());
+
+        tracker.replace(3, new Item("1001"));
+        for (int i = 0; i < tracker.size; i++) {
+            System.out.println(tracker.items[i].getName() + "\t  " + tracker.items[i].getId());
+        }
+        System.out.println();
+
+        boolean del = tracker.delete(5);
+        for (int i = 0; i < tracker.size; i++) {
+            System.out.println(tracker.items[i].getName() + "\t  " + tracker.items[i].getId() + "  " + del);
+        }
+        System.out.println();
+
+        del = tracker.delete(85);
+        for (int i = 0; i < tracker.size; i++) {
+            System.out.println(tracker.items[i].getName() + "\t  " + tracker.items[i].getId() + "  " + del);
         }
     }
 }
