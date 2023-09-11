c33b8f61e27be5ba605f7353c2e81941646b3a65
Мик <mishlmish@inbox.ru>
2023-09-11 07:18:35 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322 #444027] исправление1

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -3,14 +3,10 @@ package ru.job4j.oop;
 public class College {
     public static void main(String[] args) {
         Freshman freshman = new Freshman();
-        /* приведение к типу родителя Freshman - повышающее*/
         Student student = freshman;
-        /* приведение к типу родителя Object - повышающее*/
         Object object = freshman;
-        /* приведение при создании объекта - повышающее*/
         Student student1 = new Freshman();
         Object object1 = new Freshman();
-        /* приведение типа за счет понижения по иерархии - понижающее*/
         Student student2 = (Student) object;
     }
 }
