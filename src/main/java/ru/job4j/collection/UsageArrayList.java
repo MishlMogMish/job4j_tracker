7045bb22a0da083d49121dbaba2fbcf2d2d8963a
Мик <mishlmish@inbox.ru>
2023-10-04 21:47:04 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> collection = new ArrayList<>();
+        collection.add("Petr");
+        collection.add("Ivan");
+        collection.add("Stepan");
+        for (String string : collection) {
+            System.out.println(string);
+        }
+    }
+}
