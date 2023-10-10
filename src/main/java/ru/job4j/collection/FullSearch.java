81da23ef2d29e83eb8306f03127430fd54f8840b
Мик <mishlmish@inbox.ru>
2023-10-10 07:25:49 +0300

    1. Уникальные задачи. [#212653 #451296] исправление1

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -1,20 +1,15 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class FullSearch {
-    private final HashSet<Task> tasks = new HashSet<>();
 
-    public List<String> extractNumber(List<Task> list) {
-        tasks.addAll(list);
-
-        List<String> tempList = new ArrayList<>();
-        for (Task task : tasks) {
-            tempList.add(task.number());
+    public HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for (Task task : list) {
+            numbers.add(task.number());
         }
-        return tempList;
+        return numbers;
     }
-
 }
