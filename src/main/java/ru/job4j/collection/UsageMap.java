c737efd66566397822c53422145b75750681a016
Мик <mishlmish@inbox.ru>
2023-10-11 16:28:56 +0300

    0. Map, HashMap [#212652 #451726] исправление1

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,6 +8,8 @@ public class UsageMap {
         map.put("lobi2@list.ru", "Pavel Ivanov");
         map.put("ert777@mail.ru", "Iryna Kuzmina");
         map.put("haui@yandex.ru", "Jon Mak");
+        map.put("lobi2@list.ru", "Conan Doyle");
+        map.put("lobi2@list.ru", "Jim Kerry");
 
         for (String key : map.keySet()) {
             System.out.println(key + " = " + map.get(key));
