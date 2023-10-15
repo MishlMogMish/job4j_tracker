2a3d8c99a4ac0e14af8726bb3cd161b43077fd24
Мик <mishlmish@inbox.ru>
2023-10-15 14:21:32 +0300

    0. Сортировка [#10096 #452652]исправление1

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -1,54 +1,32 @@
 package ru.job4j.tracker;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Arrays;
 import java.util.List;
 
 public class ItemAscByNameTest {
     @Test
     public void whenSortListByAscComparator() {
-        List<Item> items = Arrays.asList(
-                new Item(1, "ffff"),
-                new Item(2, "zzz"),
-                new Item(3, "aaaaa"),
-                new Item(4, "nnnn")
-        );
+        Item item0 = new Item(0, "ffff");
+        Item item1 = new Item(1, "zzz");
+        Item item2 = new Item(2, "aaaa");
+        Item item3 = new Item(3, "nnnn");
+        List<Item> items = Arrays.asList(item0, item1, item2, item3);
         items.sort(new ItemAscByName());
-        System.out.println(items);
-        List<Item> expected = List.of(
-                new Item(3, "aaaaa"),
-                new Item(1, "ffff"),
-                new Item(4, "nnnn"),
-                new Item(2, "zzz")
-        );
-        assertThat(expected.get(0).getName()).isEqualTo(items.get(0).getName());
-        assertThat(expected.get(1).getName()).isEqualTo(items.get(1).getName());
-        assertThat(expected.get(2).getName()).isEqualTo(items.get(2).getName());
-        assertThat(expected.get(3).getName()).isEqualTo(items.get(3).getName());
+        List<Item> expected = Arrays.asList(item2, item0, item3, item1);
+        Assertions.assertArrayEquals(expected.toArray(), items.toArray());
     }
 
     @Test
     public void whenSortListByDescComparator() {
-        List<Item> items = Arrays.asList(
-                new Item(1, "ffff"),
-                new Item(2, "zzz"),
-                new Item(3, "aaaaa"),
-                new Item(4, "nnnn")
-        );
+        Item item0 = new Item(0, "ffff");
+        Item item1 = new Item(1, "zzz");
+        Item item2 = new Item(2, "aaaa");
+        Item item3 = new Item(3, "nnnn");
+        List<Item> items = Arrays.asList(item0, item1, item2, item3);
         items.sort(new ItemDescByName());
-        System.out.println(items);
-        List<Item> expected = List.of(
-                new Item(2, "zzz"),
-                new Item(4, "nnnn"),
-                new Item(1, "ffff"),
-                new Item(3, "aaaaa")
-        );
-        assertThat(expected.get(0).getName()).isEqualTo(items.get(0).getName());
-        assertThat(expected.get(1).getName()).isEqualTo(items.get(1).getName());
-        assertThat(expected.get(2).getName()).isEqualTo(items.get(2).getName());
-        assertThat(expected.get(3).getName()).isEqualTo(items.get(3).getName());
+        List<Item> expected = Arrays.asList(item1, item3, item0, item2);
+        Assertions.assertArrayEquals(expected.toArray(), items.toArray());
     }
 }
